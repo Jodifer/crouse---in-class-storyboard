1957bd6f610df140d9cc47fc8b5280740b062979
Jody Crouse <J.crouse@setonhill.edu>
2020-02-03 09:21:28 -0500

    Initial Commit

---
+++ crouse - in clASS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  crouse - in clASS
+//
+//  Created by Jody Crouse on 2/3/20.
+//  Copyright © 2020 Jody Crouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
